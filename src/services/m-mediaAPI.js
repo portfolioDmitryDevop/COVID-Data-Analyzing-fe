a4d3cbd32a79bc0b8ca32de0f381d2faab0069ef
DimaTch <4dnik@mail.ru>
2021-12-24 12:57:12 +0200

    modify data processing

--- src/services/m-mediaAPI.js
+++ src/services/m-mediaAPI.js
@@ -1,10 +1,36 @@
+
+const requestCases = "cases";
+const requestVaccines = "vaccines";
+const requestHistory = "history?status=deaths";
+
 export default class MMediaAPI {
 
     #url;
+    #historyData;
+    #vaccinesData;
 
     constructor(url) {
         if (!url) throw 'API URL is not specified.'
         this.#url = url;
+        this.#historyData = this.getData(requestHistory);
+        this.#vaccinesData = this.getData(requestVaccines);
+    }
+
+    getHistoryData(){
+        return this.#historyData;
+    }
+    getVaccinesData(){
+        return this.#vaccinesData;
+    }
+    getCasesData(){
+        return this.getData(requestCases);
+    }
+
+    async getData(parameters){
+        const response = await fetch(this.#url + parameters);
+        const res = await response.json();
+        return res;
+
     }
 
 }
