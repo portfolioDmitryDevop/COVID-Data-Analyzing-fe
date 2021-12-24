985b627636cae80db5ad186b981e3f2910941b7e
febick <febick29@gmail.com>
2021-12-24 08:45:07 +0200

    init

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+console.log("It's a live");
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  watch: true,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+      test: /\.js$/,
+      exclude: [ path.resolve(__dirname, 'node_modules')],
+      }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]}
+    ]
+  },
+  devtool: 'source-map'
+};
